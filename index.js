a9659b30039f664ded6532d28fdbdbfdb6d236bf
Milutin-S <studen.m98@gmail.com>
2024-03-04 22:42:09 +0100

    Add emojis

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Aliens!</h1>')
+  res.send('<h1>Hello Aliens! 👽 🛸</h1>')
 })
 
 const PORT = 8080
